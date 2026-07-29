5ef8879b13ed92cc838d7ebcfba28750d4308d63
WolnyJazdow <otwarty@jazdow.pl>
2026-07-29 17:10:49 +0200

    x icon

--- www/.vuepress/theme/components/oj-footer.vue
+++ www/.vuepress/theme/components/oj-footer.vue
@@ -10,7 +10,7 @@
 				h4.address-title {{$site.themeConfig.address[lang].title}}
 				.address-details(v-html="render(this.$site.themeConfig.address[lang].details)")
 		.social
-			a.social-icon(v-for="item in $site.themeConfig.social", :href="item.href", :title="item.type", target="_blank", rel="noopener", v-html="socialChar(item.type)")
+			a.social-icon(v-for="item in $site.themeConfig.social", :href="item.href", :title="item.type", target="_blank", rel="noopener") {{socialChar(item.type)}}
 		.links
 			router-link(v-for="link in $site.themeConfig.links[lang]" :key="link.to" :to="link.to") {{link.title}}
 </template>
@@ -19,8 +19,7 @@
 import markdownIt from 'markdown-it'
 const md = markdownIt({breaks: true})
 
-const xIcon = '<svg viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em" style="vertical-align:middle"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.747l7.73-8.835L1.254 2.25H8.08l4.259 5.622zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>'
-const socialChars = { facebook: 'b', instagram: 'd', youtube: 'p', linkedin: 'o', x: xIcon }
+const socialChars = { facebook: 'b', instagram: 'd', youtube: 'p', linkedin: 'o', x: 'x' }
 
 export default {
 	name: 'oj-footer',
